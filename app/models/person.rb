03f5e96a38cb8d342277cfe7effb9b8136bb2ec3
Juan Zavala <ja.zavala.aguilar@gmail.com>
2019-08-10 14:37:29 -0500

    solution to exercise and update version ruby and gems

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,10 +5,14 @@ class Person < ActiveRecord::Base
   has_many :employees, class_name: "Person", foreign_key: :manager_id
 
   def self.maximum_salary_by_location
-    {}
+    group(:location_id).maximum(:salary)
   end
 
   def self.managers_by_average_salary_difference
-    all
+    joins(
+      "INNER JOIN (" + Person.select('manager_id, AVG(salary) avg_salary').group(:manager_id).to_sql + ") salaries " +
+      "ON salaries.manager_id = people.id " +
+      "ORDER BY people.salary - salaries.avg_salary DESC"
+    )
   end
 end
